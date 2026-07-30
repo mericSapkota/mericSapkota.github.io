20bf03f08c8c3a382afb4d725146260f2b527665
Meric Sapkota <meric5sapkota@gmail.com>
2026-07-30 09:47:02 +0545

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,11 +8,8 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  // Change this to your real domain before deploying (used by the RSS feed).
-  // If hosting on GitHub Pages as '<username>.github.io/0xm3r1c-portfolio',
-  // set site to 'https://<username>.github.io' and uncomment the line below:
   site: "https://mericsapkota.me",
-  // base: '/0xm3r1c-portfolio',
+  base: "/",
 
   integrations: [react(), mdx()],
 
